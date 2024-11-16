1d7ec61efc64b0936187149c24bffe0ae31e5ce1
ramsettisuvek <ramsettisuveks@gmail.com>
2024-11-17 00:56:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import ClickCounter from './Component/ClickCounter'; // Corrected relative path with forward slashes
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="background">
+    <ClickCounter />
+  </div>
+);
 
 export default App;
